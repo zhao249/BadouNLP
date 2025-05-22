d7b34715452fcd5a1858d606ab6697af38a7dd54
Ysfasdfsfds <164968610+Ysfasdfsfds@users.noreply.github.com>
2025-05-22 15:28:17 +0800

    Add files via upload

---
+++ ys/week_02/test.py
@@ -0,0 +1,111 @@
+import torch
+import torch.nn as nn
+import numpy as np
+import random
+import json
+import matplotlib.pyplot as plt
+
+
+def generate_data():
+    x = np.random.random(5)
+    y = np.argmax(x)
+    return x,y
+
+def build_dataset(total_sample_num):
+    x_list = []
+    y_list = []
+    for i in range(total_sample_num):
+        x ,y = generate_data()
+        x_list.append(x)
+        y_list.append([y])
+
+    return torch.FloatTensor(x_list), torch.FloatTensor(y_list)
+
+
+class TorchModel(nn.Module):
+    def __init__(self, input_size):
+        super(TorchModel, self).__init__()
+        self.linear = nn.Linear(input_size, 5)  # 线性层
+        # self.activation = torch.sigmoid  # nn.Sigmoid() sigmoid归一化函数
+        self.loss = nn.CrossEntropyLoss()  # loss函数采用均方差损失
+
+    # 当输入真实标签，返回loss值；无真实标签，返回预测值
+    def forward(self, x, y=None):
+        y_pred = self.linear(x)  # (batch_size, input_size) -> (batch_size, 1)
+        # y_pred = self.activation(x)  # (batch_size, 1) -> (batch_size, 1)
+        if y is not None:
+            return self.loss(y_pred, y.long())  # 预测值和真实值计算损失
+        else:
+            return y_pred  # 输出预测结果
+        
+def evaluate(model):
+    model.eval()
+    test_sample_num = 100
+    x, y = build_dataset(test_sample_num)
+    # print("本次预测集中共有%d个正样本，%d个负样本" % (sum(y), test_sample_num - sum(y)))
+    correct, wrong = 0, 0
+    with torch.no_grad():
+        y_pred = model(x)  # 模型预测 model.forward(x)
+        for y_p, y_t in zip(y_pred, y):  # 与真实标签进行对比
+            if y_p.argmax() == y_t:
+                correct += 1  # 负样本判断正确
+            # elif float(y_p) >= 0.5 and int(y_t) == 1:
+            #     correct += 1  # 正样本判断正确
+            else:
+                wrong += 1
+    print("正确预测个数：%d, 正确率：%f" % (correct, correct / (correct + wrong)))
+    return correct / (correct + wrong)
+
+def main():
+    # 配置参数
+    epoch_num = 20  # 训练轮数
+    batch_size = 20  # 每次训练样本个数
+    train_sample = 5000  # 每轮训练总共训练的样本总数
+    input_size = 5  # 输入向量
+    learning_rate = 0.001  # 学习率
+    # 建立模型
+    model = TorchModel(input_size)
+    # 选择优化器
+    optim = torch.optim.Adam(model.parameters(), lr=learning_rate)
+    log = []
+    # 创建训练集，正常任务是读取训练集
+    train_x, train_y = build_dataset(train_sample)
+    # 训练过程
+    for epoch in range(epoch_num):
+        model.train()
+        watch_loss = []
+        for batch_index in range(train_sample // batch_size):    
+            x = train_x[batch_index * batch_size : (batch_index + 1) * batch_size,:]
+            y = train_y[batch_index * batch_size : (batch_index + 1) * batch_size,:].squeeze(1)
+            
+            loss = model(x, y)  # 计算loss  model.forward(x,y)
+            loss.backward()  # 计算梯度
+            optim.step()  # 更新权重
+            optim.zero_grad()  # 梯度归零
+            watch_loss.append(loss.item())
+        print("=========\n第%d轮平均loss:%f" % (epoch + 1, np.mean(watch_loss)))
+        acc = evaluate(model)  # 测试本轮模型结果
+        log.append([acc, float(np.mean(watch_loss))])
+    # 保存模型
+    torch.save(model.state_dict(), "model.pt")
+    # 画图
+    print(log)
+    plt.plot(range(len(log)), [l[0] for l in log], label="acc")  # 画acc曲线
+    plt.plot(range(len(log)), [l[1] for l in log], label="loss")  # 画loss曲线
+    plt.legend()
+    
+    plt.savefig("training_curve.png")  # 保存图片到当前目录
+    plt.show()
+    plt.close()
+    return
+
+
+if __name__ == "__main__":
+    # x,y = build_dataset(10)
+    # print(x)
+    # print("---------")
+    # print(y)
+    # pass
+    main()
+    
+    
